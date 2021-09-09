b2c0330469a26a62b4c13f8c42a9767c767bba15
lethanhtung <letung844gmail.com>
2021-09-09 22:27:26 +0700

    model & mongoose & config db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 async function connect() {
-
-    try{
+    try {
         await mongoose.connect('mongodb://localhost:27017/f8-education-dev');
-        console.log("done");
-    }catch(e){
-        console.log("fail");
+        console.log('done');
+    } catch (e) {
+        console.log('fail');
     }
-
 }
 
-module.exports = {connect};
+module.exports = { connect };
